62f91dc5d0712b053cdd108bad4c1de1b0c41a74
xiaomingaaa <13693963350@163.com>
2021-05-17 14:06:02 +0000

    new commit

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-python train.py --model_name TransE_l2 --hidden_dim 200 --gamma 10 --lr 0.1 --regularization_coef 1e-9 --valid --test -adv --mix_cpu_gpu --num_proc 4 --num_thread 4 --gpu 0 1 2 3 --async_update --force_sync_interval 10000 --no_save_emb --print_on_screen --encoder_model_name shallow --save_path save_path
+python train.py --model_name TransE_l2 --hidden_dim 200 --gamma 10 --lr 0.1 --regularization_coef 1e-9 --valid --test -adv --mix_cpu_gpu --num_proc 3 --num_thread 4 --gpu 1 2 3 --async_update --force_sync_interval 10000 --no_save_emb --print_on_screen --encoder_model_name roberta --save_path save_path
 
 python train.py --model_name TransE_l2 --hidden_dim 128 --gamma 10 --lr 0.1 --regularization_coef 1e-9 --valid --test -adv --mix_cpu_gpu --num_proc 4 --num_thread 4 --gpu 0 1 2 3 --async_update --force_sync_interval 10000 --no_save_emb --print_on_screen --encoder_model_name concat --save_path save_path
+
+python train.py --model_name TransE_l2 --hidden_dim 200 --gamma 10 --lr 0.1 --regularization_coef 1e-9 --valid --test -adv --mix_cpu_gpu --num_proc 4 --num_thread 4 --gpu 0 1 2 3 --async_update --force_sync_interval 10000 --no_save_emb --print_on_screen --encoder_model_name shallow --save_path save_path
+
+
 ###
  # @Author: your name
  # @Date: 2021-05-11 09:11:36
- # @LastEditTime: 2021-05-13 11:55:53
+ # @LastEditTime: 2021-05-17 03:16:22
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /wikikg90m/run.sh
